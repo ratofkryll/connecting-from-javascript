02d19bbb32a6823fb9aa4a40647d632c4f25c85d
Gioanna Rheumer <ratofkryll@gmail.com>
2018-12-19 00:58:25 +0000

    cleaned up commented out code

--- lookup_people_knex.js
+++ lookup_people_knex.js
@@ -43,37 +43,3 @@ knex.select().from('famous_people').where('first_name', search).orWhere('last_na
     });
   }
 });
-
-
-// function searchPeople (db, search) {
-//   let firstName;
-//   let lastName;
-//   let dateOfBirth;
-//
-//   db.query(`SELECT * FROM famous_people WHERE first_name ILIKE '${search}' OR last_name ILIKE '${search}';`, (err, res) => {
-//     if (err) {
-//       console.error('Search error', err);
-//     }
-//     for (let key in res.rows) {
-//       const person = res.rows[key];
-//       const date = person.birthdate.toDateString();
-//       const counter = Number(key);
-//       console.log(`- ${counter + 1}: ${person.first_name} ${person.last_name}, born ${date}`);
-//     }
-//     db.end();
-//   });
-// }
-
-// TODO: Remove below
-// client.connect((err) => {
-//   if (err) {
-//     return console.error('Connection error: ', err);
-//   }
-//   if (search === undefined) {
-//     console.log('Please enter a name to search for.');
-//   }
-//   console.log('Searching...');
-//   console.log(knex);
-//   // countResults(knex, search);
-//   // searchPeople(knex, search);
-// });
